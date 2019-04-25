b958446aa4bda936b98e223c13c3db95ce77a909
Patrick McCombs <pa.mccombs1@gmail.com>
2019-04-25 18:51:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     #your code here
     
-     if params[:username] == "" || params[:password] == ""
+    if params[:username] == "" || params[:password] == ""
       redirect '/failure'
     else
       User.create(username: params[:username], password: params[:password])
